bad0a6ab435eccda0b4d3b2f01aa6285aa412e00
George Stanciulescu <georgestanciulescu0@gmail.com>
2025-10-25 18:33:29 +0100

    Error handling etc.

--- task.cpp
+++ task.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <utility>
 #include <list>
+#include <optional>
 
 enum DetailType
 {
@@ -12,9 +13,58 @@ enum DetailType
     end,
 };
 
+std::vector<std::pair<int,std::string>> taskOrder();
+void deleteTask(std::vector<std::pair<int,std::string>>& orderedTasks,const char* task);
+void endTask(const std::vector<std::pair<int,std::string>>& orderedTasks,const char* task,const char* status,int argc);
+void addTask(char* argv[],int argc,const std::vector<std::pair<int,std::string>>& orderedTasks);
+void listTasks();
+void extraDetail(DetailType type);
+
+bool endErrorHandle(int argc,char* argv[],const std::vector<std::pair<int,std::string>>& orderedTasks);
+bool deleteErrorHandle(int argc,char* argv[],const std::vector<std::pair<int,std::string>>& orderedTasks);
+bool addErrorHandle(int argc);
+
+int main(const int argc,char* argv[])
+{
+    if (argc < 2)
+    {
+        extraDetail(basic);
+        return 1;
+    }
+
+    const std::string_view command{argv[1]}; // leave it here;if you put it at the start of main it will cause a segmentation fault
+
+    if (command == "list"){listTasks();}
+
+    std::vector orderedTasks{taskOrder()};
+
+    if (command == "add")
+    {
+        if (!addErrorHandle(argc)){return 1;}
+
+        addTask(argv,argc,orderedTasks);
+    }
+
+    if (command == "delete")
+    {
+        if (!deleteErrorHandle(argc,argv,orderedTasks)){return 1;}
+
+        deleteTask(orderedTasks,argv[2]);
+    }
+
+    if (command == "end")
+    {
+        if (!endErrorHandle(argc,argv,orderedTasks)){return 1;}
+
+        endTask(orderedTasks,argv[2],argv[3],argc);
+    }
+
+    return 0;
+}
+
 std::vector<std::pair<int,std::string>> taskOrder()
 {
-    std::ifstream myFile("first.txt");
+    std::ifstream myFile("tasks.txt");
 
     int count{0};
     std::string myTask{};
@@ -24,7 +74,7 @@ std::vector<std::pair<int,std::string>> taskOrder()
     {
         std::size_t taskName{myTask.find_first_of(' ') + 1};
         ++count;
-        std::pair<int,std::string> pair{count,myTask.substr(taskName)};
+        std::pair pair{count,myTask.substr(taskName)};
 
 
         orderedTasks.push_back(pair);
@@ -34,18 +84,16 @@ std::vector<std::pair<int,std::string>> taskOrder()
     return orderedTasks;
 }
 
-void deleteTask(const char* task)
+void deleteTask(std::vector<std::pair<int,std::string>>& orderedTasks,const char* task)
 {
     if (static_cast<std::string>(task) == "all")
     {
-        std::remove("first.txt");
+        std::remove("tasks.txt");
+        std::cout << "All the tasks have been successfully deleted!" << '\n';
         return;
     }
 
-    std::ifstream myFile("first.txt",std::ios::in);
-    std::ofstream newFile("new.txt",std::ios::out | std::ios::app);
-
-    std::vector orderedTasks{taskOrder()};
+    std::ofstream myFile("tasks.txt",std::ios::out | std::ofstream::trunc);
 
     int chosenTask{std::stoi(task)};
     --chosenTask; // Adjusting by one so that the index starts at one,not at zero
@@ -54,23 +102,18 @@ void deleteTask(const char* task)
 
     for (auto& pair : orderedTasks)
     {
-        if (pair.first <= chosenTask){newFile <<"[" << pair.first << "] " << pair.second << '\n'; }
+        if (pair.first <= chosenTask){myFile <<"[" << pair.first << "] " << pair.second << '\n'; }
 
-        else{newFile <<"[" << --pair.first << "] " << pair.second << '\n'; }
+        else{myFile <<"[" << --pair.first << "] " << pair.second << '\n'; }
     }
+    std::cout << "The task has been successfully deleted!" << '\n';
 
-    std::remove("first.txt");
-    std::rename("new.txt","first.txt");
-
-    newFile.close();
+    myFile.close();
 }
 
-void endTask(const char* task,const char* status)
+void endTask(const std::vector<std::pair<int,std::string>>& orderedTasks,const char* task,const char* status,int argc)
 {
-    std::ifstream myFile("first.txt",std::ios::in);
-    std::ofstream newFile("new.txt",std::ios::out | std::ios::app);
-
-    std::vector orderedTasks{taskOrder()};
+    std::ofstream myFile("tasks.txt",std::ios::out | std::ofstream::trunc);
 
     int chosenTask{std::stoi(task)};
     --chosenTask; // Adjusting by one so that the index starts at one,not at zero
@@ -79,36 +122,44 @@ void endTask(const char* task,const char* status)
     std::size_t taskStatus{endedTask.find_last_of(']') - 1 };
 
     endedTask.erase(taskStatus,1);
-    endedTask.insert(taskStatus,static_cast<std::string>(status));
+    endedTask.insert(taskStatus ,static_cast<std::string>(status));
+    ++chosenTask;
 
     for (const auto& pair : orderedTasks)
     {
-        if (pair.first != ++chosenTask)
+        if (pair.first != chosenTask)
         {
-            newFile <<"[" << pair.first << "] " << pair.second << '\n';
+            myFile <<"[" << pair.first  << "] " << pair.second << '\n';
 
         }
-        else{newFile <<"[" << pair.first << "] " << endedTask << '\n';}
+        else{myFile <<"[" << pair.first << "] " << endedTask << '\n';}
     }
+    std::cout << "The status of the task has been successfully updated!" << '\n';
 
-    std::remove("first.txt");
-    std::rename("new.txt","first.txt");
-
-    newFile.close();
+    myFile.close();
 }
 
-void addTask(const char* task)
+void addTask(char* argv[],int argc,const std::vector<std::pair<int,std::string>>& orderedTasks)
 {
-    std::ofstream myFile("first.txt",std::ios::app);
-
-    std::vector orderedTasks{taskOrder()};
-    myFile <<"[" << orderedTasks.size() + 1 << "] " << task << "[-]" << '\n';
+    std::ofstream myFile("tasks.txt",std::ios::app);
+    myFile <<"[" << orderedTasks.size() + 1 << "] ";
+    for (int x{2}; x<argc ;++x)
+    {
+        myFile << argv[x] << " ";
+    }
+    myFile << "[-]" << '\n';
 }
 
 void listTasks()
 {
-    std::ifstream myFile("first.txt",std::ios::in);
+    std::ifstream myFile("tasks.txt",std::ios::in);
     std::string task{};
+
+    std::cout << '\n' << "X      X  XXXXX XXXXX" << '\n' <<
+                         "X      X  X       X"   << '\n' <<
+                         "X      X  XXXXX   X"   << '\n' <<
+                         "X      X      X   X"   << '\n' <<
+                         "XXXXX  X  XXXXX   X"   << '\n';
 
     std::cout << '\n' << "-------------------" << '\n';
 
@@ -123,9 +174,15 @@ void extraDetail(DetailType type)
     switch (type)
     {
         case basic:
+            std::cout << '\n'<< "XXXXX XXXXX XXXXX  X  XXXXX X   X XXXXX" << '\n' <<
+                                "X   X X   X   X    X  X   X XX  X X"     << '\n' <<
+                                "X   X XXXXX   X    X  X   X X X X XXXXX" << '\n' <<
+                                "X   X X       X    X  X   X X  XX     X" << '\n' <<
+                                "XXXXX X       X    X  XXXXX X   X XXXXX" << '\n';
+
             std::cout << '\n' << "Options:" << '\n'
                   << "[list] -- Presents the list of tasks" << '\n'
-                  << "[add] -- Adds a task" << '\n'
+                  << "[add] -- Add a task" << '\n'
                   << "[end] -- Add a marker indicating completion,either success or failure" << '\n'
                   << "[delete] -- Delete the task completely" << "\n\n";
             break;
@@ -143,51 +200,79 @@ void extraDetail(DetailType type)
     }
 }
 
-int main(const int argc,char* argv[])
+bool endErrorHandle(int argc,char* argv[],const std::vector<std::pair<int,std::string>>& orderedTasks)
 {
-
-    if (argc < 2)
+    if (argc <4)
     {
-        extraDetail(basic);
-        return 1;
+        extraDetail(end);
+        return false;
     }
 
-    std::string_view command{argv[1]};
-
-    if (command == "add")
+    try
     {
-        if (argc <3)
+        const int taskID{std::stoi(argv[2])};
+
+        if (taskID < 1 || taskID > orderedTasks.size())
         {
-            extraDetail(add);
-            return 1;
+            std::cout << "The task ID is outside of the range! Introduce an ID for a task in the list!" << '\n';
+            return false;
         }
+    } catch (std::exception&) {
 
-        addTask(argv[2]);
+        std::cout << "You must enter the ID of the task;anything else will lead to an error!" << '\n';
+        return false;
     }
 
-    if (command == "list"){listTasks();}
+    const auto statusType{static_cast<std::string_view>(argv[3])};
 
-    if (command == "delete")
+    if (statusType != "s" && statusType != "x")
     {
-        if (argc <3)
-        {
-            extraDetail(deletion);
-            return 1;
-        }
-
-        deleteTask(argv[2]);
+        extraDetail(end);
+        return false;
     }
 
-    if (command == "end")
+    return true;
+}
+
+bool deleteErrorHandle(int argc,char* argv[],const std::vector<std::pair<int,std::string>>& orderedTasks)
+{
+    if (argc <3)
     {
-        if (argc <3)
+        extraDetail(deletion);
+        return false;
+    }
+
+    if (static_cast<std::string>(argv[2]) == "all"){return true;}
+
+    try
+    {
+        const int taskID{std::stoi(argv[2])};
+
+        if (taskID < 1 || taskID > orderedTasks.size())
         {
-            extraDetail(end);
-            return 1;
+            std::cout << "The task ID cannot be found! Introduce an ID for a task in the list!" << '\n';
+            return false;
         }
+    } catch (const std::exception&) {
 
-        endTask(argv[2],argv[3]);
+        std::cout << "You must enter the ID of the task;anything else will lead to an error!" << '\n';
+        return false;
+
     }
 
-    return 0;
+
+    return true;
+
+}
+
+bool addErrorHandle(int argc)
+{
+    if (argc <3)
+    {
+        extraDetail(add);
+        return false;
     }
+
+    return true;
+
+}
